a4c9b1f5b0ae9ee8f96af67e8051028c16730fa6
Nimra Asghar <asgharnimra1122@gmail.com>
2026-08-03 12:16:09 -0700

    Add job filtering search ordering and pagination

--- jobs/views.py
+++ jobs/views.py
@@ -1,19 +1,25 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.filters import SearchFilter, OrderingFilter
 
 from .models import Job
 from .serializers import JobSerializer
 from .permissions import IsRecruiterOwner
 from .filters import JobFilter
-from rest_framework.filters import SearchFilter
+
 
 class JobListCreateView(generics.ListCreateAPIView):
     queryset = Job.objects.all()
     serializer_class = JobSerializer
     permission_classes = [IsAuthenticated]
 
-    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filter_backends = [
+        DjangoFilterBackend,
+        SearchFilter,
+        OrderingFilter,
+    ]
+
     filterset_class = JobFilter
 
     search_fields = [
@@ -23,8 +29,19 @@ class JobListCreateView(generics.ListCreateAPIView):
         "skills",
     ]
 
+    ordering_fields = [
+        "created_at",
+        "updated_at",
+        "title",
+        "salary_min",
+        "salary_max",
+    ]
+
+    ordering = ["-created_at"]
+
     def perform_create(self, serializer):
         serializer.save(recruiter=self.request.user)
+
 
 class JobDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Job.objects.all()
